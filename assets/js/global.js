30f0235562d6a310d99aee90858e362b2f6c713a
Vinicius <vinicius.paiva2@outlook.com>
2024-10-24 10:55:36 -0300

    Adicionado favicon no global, adicionado carrinho index

--- assets/js/global.js
+++ assets/js/global.js
@@ -6,16 +6,18 @@ const globalCSS = "<link rel='stylesheet' href='/assets/css/global.css'>";
 head.innerHTML += globalCSS;
 
 const favicon = "<link rel='shortcut icon' href='/assets/img/favicon_io/favicon.ico' type='image/x-icon'>";
+head.innerHTML += favicon;
 
 const headerContent = 
 "<a href='/index.html'><img id='logo' src='/assets/img/logo.png' alt='logo hamburguer'></a>" +
 "<div id='hamburguer-icon'>" +
-"<i class='bi bi-list'></i>" +
+    "<i class='bi bi-list'></i>" +
 "</div>" +
 "<div id='nav-bar'>" +
-"<a href='/assets/html/produtos.html'>PRODUTOS</a>" +
-"<a href='/assets/html/quemsomos.html'>QUEM SOMOS</a>" +
-"<a href='/assets/html/contato.html'>CONTATO</a>" +
+    "<a href='/assets/html/produtos.html'>PRODUTOS</a>" +
+    "<a href='/assets/html/quemsomos.html'>QUEM SOMOS</a>" +
+    "<a href='/assets/html/contato.html'>CONTATO</a>" +
+    "<i class='bi bi-cart-fill'></i>" +
 "</div>";
 body.prepend(header);
 header.innerHTML += headerContent;
