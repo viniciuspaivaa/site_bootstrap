f79c6a8bb4bafecc16181129d38027f35c0e9e3c
Vinicius <vinicius.paiva2@outlook.com>
2024-10-28 10:20:27 -0300

    Footer pendente

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,6 +1,7 @@
 const head = document.getElementById('head');
 const body = document.getElementById('body');
 const header = document.createElement('header');
+const footer = document.createElement('footer');
 
 const globalCSS = "<link rel='stylesheet' href='/assets/css/global.css'>";
 head.innerHTML += globalCSS;
@@ -21,6 +22,17 @@ const headerContent =
 "</div>";
 body.prepend(header);
 header.innerHTML += headerContent;
+
+const footerContent = 
+"<a href='/index.html'><img id='logo' src='/assets/img/logo.png' alt='logo hamburguer'></a>" +
+"<p>Lorem Ipsum Lorem</p>" +
+"<div id='footer-nav-bar'>" +
+    "<a href='/assets/html/produtos.html'>PRODUTOS</a>" +
+    "<a href='/assets/html/quemsomos.html'>QUEM SOMOS</a>" +
+    "<a href='/assets/html/contato.html'>CONTATO</a>" +
+"</div>";
+body.appendChild(footer);
+footer.innerHTML += footerContent;
 
 const hamburguerMenu = document.getElementById("hamburguer-menu");
 const hamburguerIcon = document.getElementsByClassName("bi-list")[0];
